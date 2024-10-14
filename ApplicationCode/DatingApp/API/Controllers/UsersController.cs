9964d29ccdb4eb5c73d49a60b81473b38015b086
Prashant Awasthi <pawasthi9999@gmail.com>
2024-10-14 14:32:31 -0700

    Add project files.

---
+++ ApplicationCode/DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        //private readonly DataContext _context=context;
+        //public UsersController(DataContext context)
+        //{
+        //    _context = context;
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            var users = await context.Users.ToListAsync();
+            return users;
+        }
+        
+        [HttpGet("{id:int}")]   //api/Users/3
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+            return user;
+        }
+    }
+}
