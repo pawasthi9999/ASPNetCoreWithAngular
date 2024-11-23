8ee6af13c4103f7dcb2b42a0c07db9ff30f7dc5f
pawasthi9999 <129018239+pawasthi9999@users.noreply.github.com>
2024-11-22 23:09:16 -0800

    Commit after Section 4 (#4)

--- ApplicationCode/DatingApp/API/Controllers/UsersController.cs
+++ ApplicationCode/DatingApp/API/Controllers/UsersController.cs
@@ -1,27 +1,27 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController(DataContext context) : ControllerBase
+    public class UsersController(DataContext context) : BaseApiController
     {
         //private readonly DataContext _context=context;
         //public UsersController(DataContext context)
         //{
         //    _context = context;
         //}
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
             var users = await context.Users.ToListAsync();
             return users;
         }
-        
+
+        [Authorize]
         [HttpGet("{id:int}")]   //api/Users/3
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
