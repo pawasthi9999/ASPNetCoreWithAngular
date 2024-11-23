7b39e4463111367ba7ff7ca5033a073c54dad394
Prashant Awasthi <pawasthi9999@gmail.com>
2024-11-22 23:02:52 -0800

    Commit after Section 4

---
+++ ApplicationCode/DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    {
+        [HttpPost("register")] //account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) { return BadRequest("Username is taken"); }
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser() { 
+                UserName = registerDto.Username.ToLower(), 
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)), 
+                PasswordSalt = hmac.Key 
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return new UserDto { Username = registerDto.Username, Token = tokenService.CreateToken(user) };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username.ToLower());
+            if (user == null) return Unauthorized("Invalid Username");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i = 0; i < computeHash.Length; i++) { 
+                if(computeHash[i] != user.PasswordHash[i]) { return Unauthorized("Invalid Password"); }
+            }
+
+            return new UserDto() { Username=user.UserName, Token=tokenService.CreateToken(user) };
+        }
+
+        private async Task<bool> UserExists(string userName)
+        {
+            return await context.Users.AnyAsync(u => u.UserName.ToLower() == userName.ToLower());
+        }
+    }
+}
--- ApplicationCode/DatingApp/API/Entities/AppUser.cs
+++ ApplicationCode/DatingApp/API/Entities/AppUser.cs
@@ -2,7 +2,9 @@
 
 public class AppUser
 {
-public int Id { get; set; }
-public required string UserName { get; set; }
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 
 }
