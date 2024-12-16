1433f4ab5178adcaa2c0138f22a33a31b05610a6
pawasthi9999 <pawasthi9999@gmail.com>
2024-12-15 23:45:53 -0800

    Completition of Section 6 on development branch.

--- ApplicationCode/DatingApp/client/src/app/app.routes.ts
+++ ApplicationCode/DatingApp/client/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  /*This is way to protect multiple Routes with route guard. Other option is to add canActivate: [authGuard] in all routes individually*/
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent }
+    ]
+  },
+  /*This is way to protect one Route with route guard */
+  /*{ path: 'members', component: MemberListComponent, canActivate: [authGuard] },*/
+  { path: '**', component: HomeComponent, pathMatch:'full' },
+];
