25e09738c16226268083a100dcfe1a8c53bf0caa
pawasthi9999 <129018239+pawasthi9999@users.noreply.github.com>
2025-02-18 22:12:28 -0800

    End of Section 8 (#16)

---
+++ ApplicationCode/DatingApp/API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,27 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //CreateMap<AppUser, MemberDto>()
+            //    .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            //    .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            //CreateMap<Photo, PhotoDto>();
+            //CreateMap<MemberUpdateDto, AppUser>();
+            //CreateMap<RegisterDto, AppUser>();
+            //CreateMap<MessageDto, Message>();
+            //CreateMap<Message, MessageDto>()
+            //    .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+            //    .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+                .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
