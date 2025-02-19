938f292933d0f6d95fcd562d89afb286cb3c98de
pawasthi9999 <pawasthi9999@gmail.com>
2025-02-18 21:45:15 -0800

    End of Section 8

--- ApplicationCode/DatingApp/API/Program.cs
+++ ApplicationCode/DatingApp/API/Program.cs
@@ -1,6 +1,8 @@
 //using API.Data;
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 //using API.Interfaces;
 //using API.Services;
 //using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -43,4 +45,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUser(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
+
